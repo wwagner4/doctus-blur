6a54f139a305f872837f3aee048047f21de74eff
wwagner4 <wwagner4@gmail.com>
2016-04-19 08:19:05 +0200

    created first shape

--- blur-core/src/main/scala/net/entelijan/BlurTemplate.scala
+++ blur-core/src/main/scala/net/entelijan/BlurTemplate.scala
@@ -5,48 +5,73 @@ import doctus.core.util._
 import doctus.core.template._
 import doctus.core.color._
 
-case class Line(pos: DoctusPoint, weight: Double, blur: Int)
+case class Line(
+    pos: DoctusPoint,
+    length: Double,
+    weight: Double,
+    angle: Double,
+    blur: Int) {
 
+  def draw(g: DoctusGraphics): Unit = {
+    val vek = DoctusVector(0, length / 2.0).rot(angle)
+    val p1 = pos + vek
+    val p2 = pos - vek
+    val maxStrokeFac = blur + 1
+    val strokeFacs = (1 to maxStrokeFac).toList
+
+    val alphas = strokeFacs.map { _ => 255 * math.pow(2, -blur) }
+    val asum = alphas.sum
+
+    val anorm = alphas.map { a => a / asum * 255 }
+
+    strokeFacs.zip(anorm).foreach {
+      case (strokeFac, alpha) =>
+        g.stroke(DoctusColorBlack, alpha)
+        g.strokeWeight(weight + (strokeFac - 1) * 5)
+        g.line(p1, p2)
+    }
+
+  }
+
+}
+
+// TODOs
+// - scale canvas if window is scaled
 case class BlurDoctusTemplate(canvas: DoctusCanvas) extends DoctusTemplate {
-  
-  override def frameRate = None
 
   val ran = new java.util.Random
 
-  var rows = for (i <- 0 until 5) yield row(i)
+  var lines: List[Line] = createRandomLines(200)
 
-  def row(j: Int) = for (i <- 0 until 10) yield line(j, i)
+  def createRandomLines(cnt: Int): List[Line] = {
 
-  def line(col: Int, row: Int): Line = Line(DoctusPoint(30 + col * 200, (row + 1) * 45), 1 + row * 2.5, col)
+    def ranVal(max: Double) = ran.nextDouble() * max
+
+    def ranPos: DoctusPoint = DoctusPoint(ranVal(canvas.width), ranVal(canvas.height))
+
+    def ranAngle: Double = ran.nextDouble() * math.Pi
+
+    (1 to cnt).toList.map { _ =>
+      Line(ranPos, 100, 2, ranAngle, 10)
+    }
+  }
+
+  def nextLines(current: List[Line]): List[Line] = {
+    current.map { l =>
+      val angle = l.angle
+      val next = (angle + 0.1) % math.Pi
+      l.copy(angle = next)
+    }
+  }
 
   def draw(g: DoctusGraphics): Unit = {
+    lines = nextLines(lines)
+
     g.noStroke()
     g.fill(DoctusColorWhite, 255)
     g.rect(0, 0, canvas.width, canvas.height)
-
-    def drawRow(row: Seq[Line]): Unit = row.foreach { line =>
-      
-      require(line.blur >= 0)
-      
-      val x = line.pos.x
-      val y = line.pos.y
-      val maxStrokeFac = line.blur + 1
-      val strokeFacs = (1 to maxStrokeFac).toList
-      
-      val alphas = strokeFacs.map { _ => 255 * math.pow(2, -line.blur) }
-      val asum = alphas.sum
-      
-      val anorm = alphas.map { a => a / asum * 255 }
-      
-      strokeFacs.zip(anorm).foreach { case(strokeFac, alpha) =>
-        g.stroke(DoctusColorBlack, alpha)
-        g.strokeWeight(line.weight + (strokeFac - 1) * 5)
-        g.line(x, y, x, y + 40)
-      }
-      //g.fill(DoctusColorBlue, 255)
-      //g.text("%.2f %s" format(line.weight, maxStrokeFac), line.pos, 0)
-    }
-    rows.foreach { row => drawRow(row) }
+    
+    lines.foreach { l => l.draw(g) }
   }
 
   def pointableDragged(pos: DoctusPoint): Unit = () // Nothing to do here

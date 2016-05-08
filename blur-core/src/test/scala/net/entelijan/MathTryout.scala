e59a0bf9e0f9dce5bd848e8d6ba2ca7d67ac1362
wwagner4 <wwagner4@gmail.com>
2016-05-08 18:26:14 +0200

    Revert "Merge branch 't1'"
    This reverts commit f7b0023d0ea75bfded51602d441767a62bbd4915, reversing
    changes made to 1fffe679ea34068d7aec56c8fa3e5236aee69a65.

--- blur-core/src/test/scala/net/entelijan/MathTryout.scala
+++ blur-core/src/test/scala/net/entelijan/MathTryout.scala
@@ -1,17 +1,13 @@
 package net.entelijan
 
 object MathTryout extends App {
-
-  f()
-
-  def f(): Unit = {
-    (0 to (5000, 50)) foreach { x =>
-      val y = math.max(x.toDouble / 2000, 0.1) - math.pow(x.toDouble, 1.5) / 350000
-      println("%10d -> %.2f" format(x, y))
-    }
+  
+  (0 to (5000, 50)) foreach { x =>
+    val y = math.max(x.toDouble / 2000, 0.1) - math.pow(x.toDouble, 1.5) / 350000
+    println("%10d -> %.2f" format(x, y))
   }
-
-
-  // silly comment added
+  
+  
+  
   
 }

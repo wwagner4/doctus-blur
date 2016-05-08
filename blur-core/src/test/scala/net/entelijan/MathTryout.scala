972191e9d20ce53feb4934158a3b25e2f3ba8b05
wwagner4 <wwagner4@gmail.com>
2016-05-08 18:10:50 +0200

    added params

--- blur-core/src/test/scala/net/entelijan/MathTryout.scala
+++ blur-core/src/test/scala/net/entelijan/MathTryout.scala
@@ -2,9 +2,9 @@ package net.entelijan
 
 object MathTryout extends App {
 
-  f
+  f()
 
-  def f: Unit = {
+  def f(): Unit = {
     (0 to (5000, 50)) foreach { x =>
       val y = math.max(x.toDouble / 2000, 0.1) - math.pow(x.toDouble, 1.5) / 350000
       println("%10d -> %.2f" format(x, y))
